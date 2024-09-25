75f3e54b61f2a04f5127f61b4eafa26aa262002a
Travis Heavener <travis.heavener@gmail.com>
2024-09-25 12:10:43 -0400

    Updated Type class for revamped typing system

--- tlang/util/type.cpp
+++ tlang/util/type.cpp
@@ -1,36 +1,16 @@
 #include "type.hpp"
 
-// used to prevent deprecated-copy warnings (to make copy constuctor work)
-// Type& Type::operator=(const Type& t) {
-//     this->primitiveType = t.primitiveType;
-//     this->arraySizes = t.arraySizes;
-//     this->numPtrs = t.numPtrs;
-//     this->isLValue = t.isLValue;
-//     return *this;
-// }
-
 // get the total size taken up in memory by this type
 size_t Type::getSizeBytes() const {
     // memory addresses are 2 bytes on a 16-bit system such as this
-    size_t size = (this->numPtrs == 0) ? getSizeOfType(this->primitiveType) : 2;
+    size_t size = (pointers.size() == 0) ? getSizeOfType(this->primitiveType) : 2;
 
-    for (long long modifier : this->arraySizes) {
-        // not specified
-        if (modifier == TYPE_NO_ARR_SIZE)
-            size *= TYPE_NO_ARR_SIZE < 0 ? -1 : 1;
-        size *= modifier;
-    }
+    // factor in size hints (from arrays)
+    for (long long modifier : pointers)
+        if (modifier != TYPE_EMPTY_PTR)
+            size *= modifier;
 
     return size;
-}
-
-bool Type::hasEmptyArrayModifiers() const {
-    for (long long modifier : this->arraySizes)
-        if (modifier == TYPE_NO_ARR_SIZE)
-            return true;
-
-    // base case
-    return false;
 }
 
 bool Type::operator==(const Type& t) const {
@@ -38,56 +18,13 @@ bool Type::operator==(const Type& t) const {
     if (primitiveType != t.primitiveType) return false;
 
     // check num ptrs
-    if (numPtrs != t.numPtrs) return false;
+    if (pointers.size() != t.pointers.size()) return false;
 
-    // check sizes
-    if (arraySizes.size() != t.arraySizes.size()) return false;
-    
     // check each modifier
-    for (size_t i = 0; i < arraySizes.size(); ++i)
-        if (arraySizes[i] != t.arraySizes[i])
+    for (size_t i = 0; i < pointers.size(); ++i)
+        if (pointers[i] != t.pointers[i])
             return false;
-    
+
     // base case, match
     return true;
-}
-
-bool Type::checkArrayMods(const Type& t) const {
-    // check sizes
-    if (arraySizes.size() != t.arraySizes.size())
-        return false;
-    
-    // check each modifier
-    for (size_t i = 0; i < arraySizes.size(); ++i) {
-        if (i == 0) {
-            // allow empty, implied only if one has it (left-arg)
-            if (arraySizes[i] == TYPE_NO_ARR_SIZE && t.arraySizes[i] == TYPE_NO_ARR_SIZE)
-                return false;
-            else if (arraySizes[i] != TYPE_NO_ARR_SIZE && t.arraySizes[i] != TYPE_NO_ARR_SIZE && arraySizes[i] != t.arraySizes[i])
-                return false;
-        } else {
-            if (arraySizes[i] != t.arraySizes[i])
-                return false;
-        }
-    }
-    
-    // base case, match
-    return true;
-}
-
-// it works out that by just checking the size of the primitive, we can determine
-// which type to take. char & bool may be cast as int, and any may be cast as a double
-Type Type::checkDominant(Type B) const {
-    TokenType primA = this->primitiveType;
-    TokenType primB = B.primitiveType;
-
-    // if one is a pointer, just make both a pointer-sized chunk (stored as an int internally)
-    if (numPtrs > 0) return *this;
-    if (B.numPtrs > 0) return B;
-
-    if (getSizeOfType(primA) > getSizeOfType(primB))
-        return *this;
-
-    // else, assume B
-    return B;
 }
--- tlang/util/type.hpp
+++ tlang/util/type.hpp
@@ -7,50 +7,30 @@
 #include "token.hpp"
 
 // literal types
-#define TYPE_NO_ARR_SIZE -1
+#define TYPE_EMPTY_PTR 0
+
 class Type {
     public:
-        Type() : primitiveType(TokenType::VOID), arraySizes() {};
-        Type(TokenType primitiveType) : primitiveType(primitiveType), arraySizes() {};
-        // Type(const Type& t) : primitiveType(t.primitiveType), arraySizes(t.arraySizes), numPtrs(t.numPtrs), isLValue(t.isLValue) {};
+        Type() : primitiveType(TokenType::VOID), pointers() {};
+        Type(TokenType primitiveType) : primitiveType(primitiveType), pointers() {};
 
-        // Type& operator=(const Type&);
+        void addEmptyPointer() { pointers.push_back(TYPE_EMPTY_PTR); };
+        void addPointer(size_t n) { pointers.push_back(n); };
+        void popPointer() { pointers.pop_back(); };
+        size_t getNumPointers() const { return pointers.size(); };
+        bool isPointer() const { return pointers.size() > 0; };
+        void clearPtrs() { pointers.clear(); };
 
-        // for adding an array with a specified size (ie. declarations)
-        void addArrayModifier(size_t size) { arraySizes.push_back(size); };
-
-        // for adding an array without a specified size (ie. parameter type)
-        void addEmptyArrayModifier() { arraySizes.push_back(TYPE_NO_ARR_SIZE); };
-        bool hasEmptyArrayModifiers() const;
-        void popArrayModifier() { arraySizes.pop_back(); };
-        size_t getNumArrayModifiers() const { return arraySizes.size(); };
-
-        void addPointer() { ++numPtrs; };
-        void popPointer() { --numPtrs; };
-        size_t getNumPtrs() { return numPtrs; };
-
-        bool isArray() const { return arraySizes.size() > 0; };
-
-        Type checkDominant(Type B) const;
-
-        const std::vector<long long>& getArrayModifiers() const { return arraySizes; };
+        const std::vector<size_t>& getPointers() const { return pointers; };
         size_t getSizeBytes() const;
 
         TokenType getPrimitiveType() const { return primitiveType; };
 
         bool operator==(const Type&) const;
         bool operator!=(const Type& t) const { return !(*this == t); };
-        bool checkArrayMods(const Type& t) const;
-        void flipModifiers() { std::reverse(arraySizes.begin(), arraySizes.end()); };
-
-        // NOT used in EQ/NEQ comparison
-        bool getIsLValue() const { return isLValue; };
-        void setIsLValue(bool lv) { isLValue = lv; };
     private:
         TokenType primitiveType;
-        std::vector<long long> arraySizes;
-        size_t numPtrs = 0;
-        bool isLValue = false;
+        std::vector<size_t> pointers;
 };
 
 #endif
